dfa9aee3c3a4f56fb5a1dcd5d2f149b8eb14d4a8
@d.argastcev <danya.argastesv.02@mail.ru>
2021-10-01 23:13:56 +0300

    Adding static files

--- SharedWorkplace/Models/LoginModel.cs
+++ SharedWorkplace/Models/LoginModel.cs
@@ -8,24 +8,24 @@ namespace SharedWorkplace.Models
 {
     public class RegisterModel
     {
-        [Required(ErrorMessage = "Не указан Email")]
+        [Required(ErrorMessage = "Email is not specified")]
         public string Email { get; set; }
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Не указан пароль")]
+        [Required(ErrorMessage = "No password specified")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
+        [Compare("Password", ErrorMessage = "The password was entered incorrectly")]
         public string ConfirmPassword { get; set; }
     }
     public class LoginModel
     {
-        [Required(ErrorMessage = "Не указан Email")]
+        [Required(ErrorMessage = "Email is not specified")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Не указан пароль")]
+        [Required(ErrorMessage = "No password specified")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
